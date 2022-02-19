db37de9c7cd8bda4b9f75a4f53a8d1cbe7c934ce
Serafina Yu <51179577+serafinayu@users.noreply.github.com>
2022-02-19 04:12:21 -0800

    commit 3

--- HW 1/triangle.asm
+++ HW 1/triangle.asm
@@ -42,26 +42,33 @@ global triangle
 
 segment .data
 
-welMessage db "We take care of all your triangles.", 10
+string_format db "%s", 0
 
-input1prompt db "Please enter your name: "
+welMessage db "We take care of all your triangles.", 10, 0
+input1prompt db "Please enter your name: ", 0
 
-instructions1 db "Good morning "
-string_format db "%s"
-instruction2 db ", please enter the length of side 1, length of side 2, and size (degrees) of the included angle between them as real float numbers. Separate the numbers by white space, and be sure to press <enter> after the last inputted number.", 10
+instructions1 db "Good morning ", 0
+instructions2 db ",please enter the length of side 1," 
+              db "length of side 2, and size (degrees) of the included" 
+              db "angle between them as real float numbers. Separate the" 
+              db "numbers by white space, and be sure to press <enter> after" 
+              db "the last inputted number.", 10, 0
 
-input2prompt db 
+three_float_format db "%lf %lf %lf", 0
 
-one_float_format db "%lf", 0
+thankYou db "Thank you ", 0
+output_floats db ". You entered %1.7lf %1.7lf %1.7lf", 10, 0
 
-output_floats_inputted db "You entered %.10lf  %.10lf  %.10lf", 10, 0
-output_area_float db "The area of your triangle is %.10lf", 10, 0
-output_perimeter_float db "The perimeter is %.10lf", 10, 0
+output_area_float db "The area of your triangle is %1.7lf", 10, 0
+output_perimeter_float db "The perimeter is %1.7lf", 10, 0
+endMess db "The area will now be sent to the driver function.", 10, 0
 
 
 
 segment .bss
-; Empty segmentL there are no un-initialized arrays.
+;Create an empty string array that will hold the name of the user
+full_name resb 32
+
 
 
 segment .text
@@ -85,20 +92,156 @@ push r15                                                    ;Backup r15
 push rbx                                                    ;Backup rbx
 push qword -1  
 
-;Output welcome message
-mov rax
+;===== Output welcome message ===============================================================================
 
-mov rax,3
-mov rdi, three_float_format
-mov rsi, rspP
-call scanf
-movsd xmm10, [rsp]
+push qword 0
+mov rax, 0
+mov rdi, welMessage
+call printf
+
+;===== Ask user for his/her name ============================================================================
+
+push qword 0
+mov rax, 0
+mov rdi, input1prompt
+call printf
+
+;===== Obtain users full name ===============================================================================
+
+push qword 0
+mov rdi, full_name
+mov rsi, 32                     ;Tell rdi that full_name will have a max storage value of 32 bytes
+mov rdx, [stdin]                ;Let fgets know where the data is coming from (comes from keyboard [stdin])
+call fgets
+;added this below from SI notes (go over what it means)
+; mov rax, 0
+; mov rdi, number
+; call strlen
+; sub rax, 1
+; mov byte[full_name+rax], 0
+
+;===== Get # of chars in full_name ==========================================================================
+
+push qword 0
+mov rax, 0
+mov rdi, full_name
+call strlen
+mov r14, rax                    ;r14 holds the # of chars
+
+;===== Output instructions ==================================================================================
+
+push qword 0
+mov rax, 0
+mov rdi, instructions1  
+call printf
+
+;Print the name after instructions1
+push qword 0
+mov rax, 0                    
+mov [full_name+r14-1], '\0'
+mov rdi, string_format          ;"%s"
+mov rsi, full_name              ;full_name = user name w/o the new line 
+call printf
+
+;Output instructions2
+push qword 0
+mov rax, 0
+mov rdi, instruction2
+call printf
+
+;===== Obtain user's choice of 2 side lengths and angle in degrees ==========================================
+
+mov rax, 3                      ;Returning a value (3 floats) to the call function scanf
+mov rdi, three_float_format     ;"%lf %lf %lf"
+push qword 0                    ;Push qword 0, 3 times since we're adding 3 numbers to the stack
+mov rsi, rsp                    ;Moves rsp down one 
+push qword 0
+mov rdx, rsp
+push qword 0
+mov rcx, rsp
+call scanf                      ;Call scanf to read in the values
+
+movsd xmm13, [rsp]              ;Moves the third inputted number (which is the angle) into xmm13 
+pop rax                         ;Get rid of this part of the stack so that rsp points to the 2nd float
+movsd xmm14, [rsp]              ;Moves the second inputted float into xmm14
 pop rax
-movsd xmm11, [rsp]
-pop rax
-movsd xmm12, [rsp]
+movsd xmm15, [rsp]              ;Moves the first inputted float into xmm15
 pop rax
 
+;===== Output thank you message==============================================================================
+
+push qword 0
+mov rax, 0
+mov rdi, thankYou
+call printf
+
+;Print name after thank you message
+push qword 0 
+mov rax, 0 
+mov rdi, string_format
+mov rsi, full_name
+call printf
+
+;Print output statement
+push qword 0
+mov rax, 3
+mov rdi, output_floats          ;". You entered %1.7lf %1.7lf %1.7lf"
+movsd xmm0, xmm15               ;Store side1 into xmm0
+movsd xmm1, xmm14               ;Store side2 into xmm1
+movsd xmm2, xmm13               ;Store angle into xmm2
+call printf                 
+pop rax 
+
+;===== Calculate area of triangle ==========================================================================
+
+;Formula for area = (1/2)ab(sin(C))
+
+;xmm0 = side1, area
+;xmm1 = side2
+;xmm2 = angle, sin(angle)
+;xmm12 = 1/2
+
+push qword 0
+mov rax, 1
+mov rdi, output_area_float
+movsd xmm12, 0.5
+mulsd xmm2, sin(xmm2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+;************************************************************************************************************
+;Obtain from user 2 side length & angle
+mov rax, 3                      ;3 since we're using 3 floats
+mov rdi, three_float_input      ;Tell rdi we're inputting it into the 3 float input stencil
+push qword 0                    ;Push qword 0, 3 times since we're adding 3 numbers to the stack
+mov rsi, rsp                    ;Moves rsp down one 
+push qword 0
+mov rdx, rsp
+push qword 0
+mov rcx, rsp
+call scanf                      ;Call scanf to read in the values
+
+movsd xmm13, [rsp]              ;Moves the third inputted number (which is the angle) into xmm13 
+pop rax                         ;Get rid of this part of the stack so that rsp points to the 2nd float
+movsd xmm14, [rsp]              ;Moves the second inputted float into xmm14
+pop rax
+movsd xmm15, [rsp]              ;Moves the first inputted float into xmm15
+pop rax
+
+
+;Print thank you statement
+mov rax, 0
+mov rdi, input2prompt
 
 ;mov rdi, three_float_format
 push qword 99
--- HW 1/triangleDriver.cpp
+++ HW 1/triangleDriver.cpp
@@ -6,12 +6,13 @@ extern "C" double area();
 
 int main()
 {
+    double calcArea = 0.0
     printf("Welcome to Amazing Triangles programmed Serafina Yu on February 15, 2022\n");
-
-    double calcArea = area();
     
-    printf("The driver received this number %f square units", calcArea);
-    printf("An integer zero will now be sent to the operating system.  Have a good day.  Bye\n");
+    calcArea = area();
+    
+    printf("The driver received this number %1.9f square units", calcArea);
+    printf("An integer zero will now be sent to the operating system. Have a good day. Bye\n");
     return 0;
 
 }
